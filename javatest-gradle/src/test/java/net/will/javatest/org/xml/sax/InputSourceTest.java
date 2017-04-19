4b14be2ae3a70be4bddfb97821c48f362a009e63
Will Yuan <will.yuan@ericsson.com>
2017-04-19 11:52:26 +0800

    Introduce TestNG.
    - Introduce TestNG.
    - Add a test case for sax resolving.

---
+++ javatest-gradle/src/test/java/net/will/javatest/org/xml/sax/InputSourceTest.java
@@ -0,0 +1,29 @@
+package net.will.javatest.org.xml.sax;
+
+import static org.testng.Assert.*;
+
+import java.io.StringReader;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.testng.annotations.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.InputSource;
+
+public class InputSourceTest {
+    @Test
+    public void test01() throws Exception {
+        String xml = "<QueryResult >Asset</QueryResult>";
+        DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+        InputSource is = new InputSource(new StringReader(xml.trim()));
+        Document doc = builder.parse(is);
+        
+        assertNotNull(doc);
+        Element elm = doc.getDocumentElement();
+        assertNotNull(elm);
+        
+        assertEquals(elm.getTextContent(), "Asset");
+    }
+}
