e5806c7b7e72e2d517c0d2782e5067ad1ffdc7d3
Will <yydragon.000@gmail.com>
2018-10-23 16:34:25 +0800

    tag

--- javatest-gradle/src/main/java/net/will/javatest/basicconcept/ChildClazz.java
+++ javatest-gradle/src/main/java/net/will/javatest/basicconcept/ChildClazz.java
@@ -3,6 +3,7 @@ package net.will.javatest.basicconcept;
 public class ChildClazz extends FatherClazz {
     
     public ChildClazz() {
+        super("pass-in-param-from-child");
         System.out.println("This is in ChildClazz.");
     }
     
--- javatest-gradle/src/main/java/net/will/javatest/basicconcept/FatherClazz.java
+++ javatest-gradle/src/main/java/net/will/javatest/basicconcept/FatherClazz.java
@@ -4,4 +4,8 @@ public class FatherClazz {
     public FatherClazz() {
         System.out.println("This is in FatherClazz.");
     }
+    
+    public FatherClazz(String arg) {
+        System.out.println(String.format("This is in FatherClazz Constructor with argument: %s", arg));
+    }
 }
